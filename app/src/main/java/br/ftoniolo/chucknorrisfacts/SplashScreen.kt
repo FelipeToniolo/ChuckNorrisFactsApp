16b83aa04e530106ae39f99f0bcbc7843a9d09c0
ftoniolo <ftoniolo@braspag.com.br>
2021-09-22 19:42:36 -0300

    finished splash screen with animation

---
+++ app/src/main/java/br/ftoniolo/chucknorrisfacts/SplashScreen.kt
@@ -0,0 +1,36 @@
+package br.ftoniolo.chucknorrisfacts
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.constraintlayout.motion.widget.MotionLayout
+import br.ftoniolo.chucknorrisfacts.databinding.ActivitySplashScreenBinding
+
+class SplashScreen : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySplashScreenBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySplashScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        supportActionBar!!.hide()
+
+        binding.screenMotion.setTransitionListener(object : MotionLayout.TransitionListener{
+            override fun onTransitionStarted(motionLayout: MotionLayout?, startId: Int, endId: Int) {
+            }
+
+            override fun onTransitionChange(motionLayout: MotionLayout?, startId: Int, endId: Int, progress: Float) {
+            }
+
+            override fun onTransitionCompleted(motionLayout: MotionLayout?, currentId: Int) {
+                val intent = Intent(this@SplashScreen, MainActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+
+            override fun onTransitionTrigger(motionLayout: MotionLayout?, triggerId: Int, positive: Boolean, progress: Float) {
+            }
+        })
+    }
+}
