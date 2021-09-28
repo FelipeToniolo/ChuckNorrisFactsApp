43f8fb0f2a63ae55f776169ff2143eb8e47b2379
ftoniolo <ftoniolo@braspag.com.br>
2021-09-28 20:12:30 -0300

    add binding in FactsActivity

--- app/src/main/java/br/ftoniolo/chucknorrisfacts/FactsActivity.kt
+++ app/src/main/java/br/ftoniolo/chucknorrisfacts/FactsActivity.kt
@@ -2,10 +2,16 @@ package br.ftoniolo.chucknorrisfacts
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import br.ftoniolo.chucknorrisfacts.databinding.ActivityFactsBinding
 
 class FactsActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityFactsBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_facts)
+        val binding = ActivityFactsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        supportActionBar!!.hide()
     }
 }
